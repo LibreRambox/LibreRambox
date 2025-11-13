e7879cb0089e199ac5ec1e454e91cd7da14f33bc
Kevin R <dev@roebert.eu>
2025-11-13 22:06:48 +0100

    Updated user agent
    WhatsApp requires Chrome >= 80

--- electron/main.js
+++ electron/main.js
@@ -701,7 +701,7 @@ app.on('web-contents-created', (event, contents) => {
 		},
 		(details, callback) => {
 			details.requestHeaders['User-Agent'] =
-				'Mozilla/5.0 (X11; Linux x86_64; rv:128.1.0) Gecko/20100101 Firefox/128.1.0';
+				'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36';
 			callback({ requestHeaders: details.requestHeaders });
 		}
 	);
